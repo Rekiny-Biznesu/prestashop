134f74e8ba850379ac02ff96babf69ab83b6b46f
HM158 <hmaszlanka@gmail.com>
2021-11-19 21:41:50 +0100

    Tłumaczenia 2

---
+++ modules/ps_mbo/translations/pl.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ps_mbo}prestashop>ps_mbo_42667b4f4303551b1d51722fe58bf14c'] = 'Prestashop w BackOfiice';
+$_MODULE['<{ps_mbo}prestashop>ps_mbo_598771837d403181383aa25c5b3ca84c'] = 'Odkryj najlepsze moduły do optymalizacji sklepu';
